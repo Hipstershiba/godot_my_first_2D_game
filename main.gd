5a8a3bcc526b88490236e39d356ab08211785475
hipstershiba <vitorrizzato@gmail.com>
2025-07-11 10:56:00 -0300

    The main game scene 3-3
    add the "Testing the scene" substep

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
